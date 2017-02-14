f2cbd07c1b641e16491f4db2a08f2a9043f3a64b
oibitoye <ibitoyeolawale@gmail.com>
2017-02-14 15:33:36 +0100

    added some functions

--- app/reverse.js
+++ app/reverse.js
@@ -1,4 +1,35 @@
 /*
-Write a function *reverseString* that takes in a string argument and returns the reverse of the string provided.
-If the reverse of the string is the same as the original string, as in the case of palindromes, return true instead.
+Write a function *reverseString* that takes in a string
+argument and returns the reverse of the string provided.
+If the reverse of the string is the same as the original
+string, as in the case of palindromes, return true instead.
 */
+
+function reverseString(input) {
+	//var split_input = input.split("")
+	switch (input) {
+		case (testPalindromes(input) === 1): return_val = true;
+		break;
+
+		case " ": return_val = null;
+		break;
+
+		default: {
+			return_val = revWords(input);
+		}
+
+	} return return_val;
+}
+
+function revWords(str){
+    return str.split("").reverse().join("");
+}
+
+function testPalindromes(word) {
+	if (revWords(word) === word) {
+		return 1;
+	}
+
+}
+
+exports.reverseString = reverseString;
--- app/word-app.js
+++ app/word-app.js
@@ -1,9 +1,34 @@
 /*
-Write a function called *words* that takes in a string argument and counts the occurrences of each word in the string.
-The function should return a JSON object showing the words identified, and their occurrences.
-For example for the input `olly olly in come free`, the output should be
+Write a function called *words* that takes in a string
+argument and counts the occurrences of each word in the string.
+The function should return a JSON object showing the words
+identified, and their occurrences.
+For example for the input `olly olly in come free`,
+the output should be
 >olly: 2
 >in: 1
 >come: 1
 >free: 1
 */
+
+function words(inputword) {
+  var input_word = inputword.replace(/\r\n\t/, " ");
+  const space_count = input_word.split(" ");
+  const word_count = {};
+  var i = 0;
+
+  while (i < space_count.length) {
+
+    if (word_count[space_count[i]] === undefined) {
+      word_count[space_count[i]] = 1;
+
+    } else {
+      word_count[space_count[i]] ++;
+    } 
+    i++;
+
+  } return word_count;
+}
+
+exports.words = words;
+
